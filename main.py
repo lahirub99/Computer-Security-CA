2ed8534ba613e247c232b2107b1f0e92f6d4d6b5
Lahiru Bandara <lahirub@ceyentra.com>
2023-10-16 16:43:58 +0530

    Staff registration and input password hiding

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import hashlib
+import getpass
 
 # Hash passwords using MD5
 def hash_password(password):
@@ -7,6 +8,15 @@ def hash_password(password):
 ''' 
 config file: users.csv
 Columns: username, password_hash, user_type, privilege_level
+
+user_type: patient, doctor, nurse, receptionist, pharmacist, lab technician
+privilage_level: 
+    patient: 0
+    doctor: 1
+    nurse: 2
+    receptionist: 3
+    pharmacist: 4
+    lab technician: 5
 '''
 '''
 config file: patientdata.csv
@@ -16,6 +26,9 @@ def register():
     print("\n#### Registration section ####")
     username = input("Username: ").strip()
 
+    # Todo - Check whether username already exists 
+
+
     print("\nSelect the user type:\n1-Patient\n2-Hospital staff\n")
     user_type = input("Your type:")
 
@@ -23,14 +36,59 @@ def register():
         user_type = "patient"
         privilage_level = "0"
     
+    elif user_type == "2":
+        # Varification codes for staff types has been used for improved security.
+        varification_codes = { "1": "doc111", 
+                              "2": "nur222", 
+                              "3": "rec333", 
+                              "4": "pha444", 
+                              "5": "lab555" }
+        
+        print("\nSelect staff type:\n1-Doctor\n2-Nurse\n3-Receptionist\n4-Pharmacist\n5-Lab technician\n")
+        staff_type = input("Your type:").strip()
+        code = input("\nEnter the varification code:")
+
+        
+        if code == varification_codes[staff_type]:
+                if staff_type == "1":
+                    user_type = "doctor"
+                    privilage_level = "1"
+                elif staff_type == "2":
+                    user_type = "nurse"
+                    privilage_level = "2"
+                elif staff_type == "3":
+                    user_type = "receptionist"
+                    privilage_level = "3"
+                elif staff_type == "4":
+                    user_type = "pharmacist"
+                    privilage_level = "4"
+                elif staff_type == "5":
+                    user_type = "lab technician"
+                    privilage_level = "5"
+                else:
+                    print("Invalid input for the staff type. Please check again.")
+                    return
+        else: 
+            print("Varification code is worng. Please check again.")
+            return
+    else:
+        print("Invalid input for the user type. Please check again.")
+        return
+        
     # Password configuration
     while True:
-        password = input("Password: ").strip()
+        # password = input("\nPassword: ").strip()
+        # Password that user input will be hidden using the getpass library for improved secriuty.
+        password = getpass.getpass("\nPassword: ")
+
         # Password should be at least 8 characters for enhanced security.
         if len(password) < 8:
             print("Password should be at least 8 characters. Re-enter password.")
         else:
-            confirm_password = input("Confirm password: ").strip()
+            # confirm_password = input("Confirm password: ").strip()
+            # Password that user input will be hidden using the getpass library for improved secriuty.
+            confirm_password = getpass.getpass("Confirm password: ")
+
             if password != confirm_password:
                 print("Passwords do not match. Re-enter password.")
             else:
